b95ae6d561cc852c5930455768495c4c8de5186c
hewp84 <98930042+hewp84@users.noreply.github.com>
2023-02-27 10:02:23 -0600

    updating lesson 9

---
+++ circle.py
@@ -0,0 +1,14 @@
+#Copy paste these functions and save them as a python file called circle.py. The name of the file determines the name of the library
+import math
+
+def area(r):
+    return math.pi * r ** 2
+
+def circumference(r):
+    return math.pi * r * 2
+
+def sphere(r):
+    return 4/3 * math.pi * r ** 3
+
+def cs103():
+    print('Have a great weekend!')
